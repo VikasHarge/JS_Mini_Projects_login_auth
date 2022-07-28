247b2e37e762d75cc6a5a4fd5e679287c24799e5
vikas harge <harge.vikas@gmail.com>
2022-07-28 23:17:41 +0530

    media qury doone

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-// ================ toggle singup login =========== //
+// ================ toggle between singup login =========== //
 
 const singup_togg = document.getElementById('singUp-togg');
 const login_togg = document.getElementById('logIn-togg');
 const dashboard = document.getElementById('login_dashboard');
+const welcome_page_1 = document.getElementById('welcome_page_1');
+const welcome_greeting = document.getElementById('welcome_greeting');
+
+
+
 
 function loginChange(){
     login_togg.style.display = 'none';
@@ -41,21 +46,28 @@ function singup(){
     else if (password_s === password2_s){
         alert("✔️ account created sucessfully !, redirecting to login");
         localStorage.setItem('firstName_local', firstName_s);
-        localStorage.setItem('lastName-local', lastName_s);
+        localStorage.setItem('lastName_local', lastName_s);
         localStorage.setItem('email_local', email_s);
         localStorage.setItem('pass_local', password_s);
         localStorage.setItem('pass2_local', password2_s);
         loginChange();
         pageChange();
     }
-
-
-    
 }
 
 
 
 // ====================== login ======================= //
+
+// ========= welcome greetings ========== //
+
+function greetingFn(){
+    var fName = localStorage.getItem('firstName_local');
+    var lName = localStorage.getItem('lastName_local');
+    var fullName = fName + " " + lName;
+    welcome_greeting.innerText = fullName;
+    welcome_greeting.style.fontSize = "1.5rem"
+}
 
 function login(){
     
@@ -69,6 +81,7 @@ function login(){
         login_togg.style.display = 'none';
         singup_togg.style.display = 'none';
         dashboard.style.display = "flex"
+        greetingFn();
     } else if(email_l !== email_local || password2_l !== pass_local) {
         const err_msg = document.getElementById('error_msg_1');
         err_msg.classList.toggle('msg_style');
@@ -76,3 +89,90 @@ function login(){
     } 
 
 }
+
+
+// ======================== logOut ================== //
+
+function logOut (){
+    window.location.reload();
+}
+
+
+
+// ====================== save login data and check for user =================== //
+
+
+// let submit = document.getElementById('submit');
+
+// submit.addEventListener('click', singup)
+
+// let arr=JSON.parse(localStorage.getItem("person")) || [];
+
+// function singup(event){
+//     event.preventDefault();
+    
+//     let obj = {
+//         firstName: document.getElementById('firstName').value,
+//         lastName: document.getElementById('firstName').value,
+//         Email : document.getElementById('email').value,
+//         pass1 : document.getElementById('password').value,
+//         pass2 : document.getElementById('password2').value
+//     }
+
+//     // check2(obj.pass1, obj.pass2);
+
+//     console.log(arr.filter('pass1'));
+
+
+//     if(check(obj.Email)===true){
+//         arr.push(obj);
+//         localStorage.setItem("person", JSON.stringify(arr));
+//     } else {
+//         alert("account already exist");
+//     }
+
+    // ======= conditions ========== //
+
+
+     
+    // else if (password_s !== password2_s) {
+    //     document.getElementById('password2').style.border = "1px solid red";
+
+    // } 
+    // else if (password_s === password2_s){
+    //     alert("✔️ account created sucessfully !, redirecting to login");
+    //     localStorage.setItem('firstName_local', firstName_s);
+    //     localStorage.setItem('lastName_local', lastName_s);
+    //     localStorage.setItem('email_local', email_s);
+    //     localStorage.setItem('pass_local', password_s);
+    //     localStorage.setItem('pass2_local', password2_s);
+    //     loginChange();
+    //     pageChange();
+    // }
+// }
+
+
+// function check(email){
+//     let filter = arr.filter(function(ele){
+//         return email === ele.Email;
+//     })
+
+//     if(filter.length > 0){
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+
+// function check2(pass_1, pass_2){
+
+//     let filter1 = arr.filter(pass_1)
+//     let filter2 = arr.filter(pass_2)
+
+//     if(filter1 !== filter2){
+//         alert('no mathch')
+//     }
+
+    
+// }
+
