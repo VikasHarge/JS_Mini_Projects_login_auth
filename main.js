ead78778e2a591e78cc9e48ed0515cfe11911795
vikas harge <harge.vikas@gmail.com>
2022-07-28 11:38:14 +0530

    login sin=gnup form done

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 
+// ================ toggle singup login =========== //
+
+const singup = document.getElementById('singUp-togg');
+ login = document.getElementById('logIn-togg')
+
+
+
+
+
+function loginChange(){
+    login.style.display = 'none';
+    singup.style.display = 'flex';
+}
+
+function pageChange(){
+    login.style.display = 'flex';
+    singup.style.display = 'none';
+}
