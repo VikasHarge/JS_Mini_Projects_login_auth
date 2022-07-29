eb94844b1e3dc65c91dfda80a1a1393d04c368e7
vikas harge <harge.vikas@gmail.com>
2022-07-29 20:43:55 +0530

    updated code

--- main.js
+++ main.js
@@ -22,112 +22,12 @@ function pageChange(){
 }
 
 
-//==================== sing up ==============  //
-
-// function singup(){
-
-//     // ========= data from from ========== //
-
-//     var firstName_s = document.getElementById('firstName').value;
-//     var lastName_s = document.getElementById('lastName').value;
-//     var email_s = document.getElementById('email').value;
-//     var password_s = document.getElementById('password').value;
-//     var password2_s = document.getElementById('password2').value;
-
-
-//     // ============= store to local storage ========//
-
-//     if(firstName_s === "" || lastName_s === "" || email_s === "" || password_s === "" || password2_s === "" ){
-//         alert("❌ Please fill, all the fields");
-//     }  
-//     else if (password_s !== password2_s) {
-//         document.getElementById('password2').style.border = "1px solid red";
-
-//     } 
-//     else if (password_s === password2_s){
-//         alert("✔️ account created sucessfully !, redirecting to login");
-//         localStorage.setItem('firstName_local', firstName_s);
-//         localStorage.setItem('lastName_local', lastName_s);
-//         localStorage.setItem('email_local', email_s);
-//         localStorage.setItem('pass_local', password_s);
-//         localStorage.setItem('pass2_local', password2_s);
-//         loginChange();
-//         pageChange();
-//     }
-// }
-
-
-
-// ====================== login ======================= //
-
-// ========= welcome greetings ========== //
-
-
-
-// function login(){
-    
-//     var email_l = document.getElementById('email_L').value;
-//     var password2_l = document.getElementById('password_L').value;
-
-//     var email_local = localStorage.getItem("email_local");
-//     var pass_local = localStorage.getItem("pass2_local");
-
-//     if(email_l === email_local && password2_l === pass_local){
-//         login_togg.style.display = 'none';
-//         singup_togg.style.display = 'none';
-//         dashboard.style.display = "flex"
-//         greetingFn();
-//     } else if(email_l !== email_local || password2_l !== pass_local) {
-//         const err_msg = document.getElementById('error_msg_1');
-//         err_msg.classList.toggle('msg_style');
-//         err_msg.innerHTML=" ❌ Wrong Email and password";
-//     } 
-
-// }
-
-
-// ======================== logOut ================== //
-
-function logOut (){
-    window.location.reload();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // ====================== save login data and check for user =================== //
 
 
-// let submit = document.getElementById('submit');
+let submit = document.getElementById('submit');
 
-// submit.addEventListener('click', singup)
+submit.addEventListener('click', singup)
 
 let arr=JSON.parse(localStorage.getItem("person")) || [];
 
@@ -177,8 +77,6 @@ function check(email){
 
 
 // ================================= log in ================================== //
-
-
 
 
 function login(){
@@ -235,8 +133,15 @@ function login(){
 
 
 function greetingFn(fName, lName){
-
     var fullName = fName + " " + lName;
     welcome_greeting.innerText = fullName;
     welcome_greeting.style.fontSize = "1.5rem"
 }
+
+
+// ======================== logOut ================== //
+
+function logOut (){
+    window.location.reload();
+}
+
