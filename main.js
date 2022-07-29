06eed0841d0ed4d140becbaca526202d27e88a9b
vikas harge <harge.vikas@gmail.com>
2022-07-29 19:39:30 +0530

    login singup done

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const welcome_greeting = document.getElementById('welcome_greeting');
 
 
 
+
 function loginChange(){
     login_togg.style.display = 'none';
     singup_togg.style.display = 'flex';
@@ -23,37 +24,37 @@ function pageChange(){
 
 //==================== sing up ==============  //
 
-function singup(){
+// function singup(){
 
-    // ========= data from from ========== //
+//     // ========= data from from ========== //
 
-    var firstName_s = document.getElementById('firstName').value;
-    var lastName_s = document.getElementById('lastName').value;
-    var email_s = document.getElementById('email').value;
-    var password_s = document.getElementById('password').value;
-    var password2_s = document.getElementById('password2').value;
+//     var firstName_s = document.getElementById('firstName').value;
+//     var lastName_s = document.getElementById('lastName').value;
+//     var email_s = document.getElementById('email').value;
+//     var password_s = document.getElementById('password').value;
+//     var password2_s = document.getElementById('password2').value;
 
 
-    // ============= store to local storage ========//
+//     // ============= store to local storage ========//
 
-    if(firstName_s === "" || lastName_s === "" || email_s === "" || password_s === "" || password2_s === "" ){
-        alert("❌ Please fill, all the fields");
-    }  
-    else if (password_s !== password2_s) {
-        document.getElementById('password2').style.border = "1px solid red";
+//     if(firstName_s === "" || lastName_s === "" || email_s === "" || password_s === "" || password2_s === "" ){
+//         alert("❌ Please fill, all the fields");
+//     }  
+//     else if (password_s !== password2_s) {
+//         document.getElementById('password2').style.border = "1px solid red";
 
-    } 
-    else if (password_s === password2_s){
-        alert("✔️ account created sucessfully !, redirecting to login");
-        localStorage.setItem('firstName_local', firstName_s);
-        localStorage.setItem('lastName_local', lastName_s);
-        localStorage.setItem('email_local', email_s);
-        localStorage.setItem('pass_local', password_s);
-        localStorage.setItem('pass2_local', password2_s);
-        loginChange();
-        pageChange();
-    }
-}
+//     } 
+//     else if (password_s === password2_s){
+//         alert("✔️ account created sucessfully !, redirecting to login");
+//         localStorage.setItem('firstName_local', firstName_s);
+//         localStorage.setItem('lastName_local', lastName_s);
+//         localStorage.setItem('email_local', email_s);
+//         localStorage.setItem('pass_local', password_s);
+//         localStorage.setItem('pass2_local', password2_s);
+//         loginChange();
+//         pageChange();
+//     }
+// }
 
 
 
@@ -61,34 +62,28 @@ function singup(){
 
 // ========= welcome greetings ========== //
 
-function greetingFn(){
-    var fName = localStorage.getItem('firstName_local');
-    var lName = localStorage.getItem('lastName_local');
-    var fullName = fName + " " + lName;
-    welcome_greeting.innerText = fullName;
-    welcome_greeting.style.fontSize = "1.5rem"
-}
 
-function login(){
+
+// function login(){
     
-    var email_l = document.getElementById('email_L').value;
-    var password2_l = document.getElementById('password_L').value;
+//     var email_l = document.getElementById('email_L').value;
+//     var password2_l = document.getElementById('password_L').value;
 
-    var email_local = localStorage.getItem("email_local");
-    var pass_local = localStorage.getItem("pass2_local");
+//     var email_local = localStorage.getItem("email_local");
+//     var pass_local = localStorage.getItem("pass2_local");
 
-    if(email_l === email_local && password2_l === pass_local){
-        login_togg.style.display = 'none';
-        singup_togg.style.display = 'none';
-        dashboard.style.display = "flex"
-        greetingFn();
-    } else if(email_l !== email_local || password2_l !== pass_local) {
-        const err_msg = document.getElementById('error_msg_1');
-        err_msg.classList.toggle('msg_style');
-        err_msg.innerHTML=" ❌ Wrong Email and password";
-    } 
+//     if(email_l === email_local && password2_l === pass_local){
+//         login_togg.style.display = 'none';
+//         singup_togg.style.display = 'none';
+//         dashboard.style.display = "flex"
+//         greetingFn();
+//     } else if(email_l !== email_local || password2_l !== pass_local) {
+//         const err_msg = document.getElementById('error_msg_1');
+//         err_msg.classList.toggle('msg_style');
+//         err_msg.innerHTML=" ❌ Wrong Email and password";
+//     } 
 
-}
+// }
 
 
 // ======================== logOut ================== //
@@ -134,73 +129,114 @@ function logOut (){
 
 // submit.addEventListener('click', singup)
 
-// let arr=JSON.parse(localStorage.getItem("person")) || [];
+let arr=JSON.parse(localStorage.getItem("person")) || [];
 
-// function singup(event){
-//     event.preventDefault();
+function singup(event){
+    event.preventDefault();
     
-//     let obj = {
-//         firstName: document.getElementById('firstName').value,
-//         lastName: document.getElementById('firstName').value,
-//         Email : document.getElementById('email').value,
-//         pass1 : document.getElementById('password').value,
-//         pass2 : document.getElementById('password2').value
-//     }
-
-//     // check2(obj.pass1, obj.pass2);
-
-//     console.log(arr.filter('pass1'));
-
-
-//     if(check(obj.Email)===true){
-//         arr.push(obj);
-//         localStorage.setItem("person", JSON.stringify(arr));
-//     } else {
-//         alert("account already exist");
-//     }
-
-    // ======= conditions ========== //
-
-
-     
-    // else if (password_s !== password2_s) {
-    //     document.getElementById('password2').style.border = "1px solid red";
-
-    // } 
-    // else if (password_s === password2_s){
-    //     alert("✔️ account created sucessfully !, redirecting to login");
-    //     localStorage.setItem('firstName_local', firstName_s);
-    //     localStorage.setItem('lastName_local', lastName_s);
-    //     localStorage.setItem('email_local', email_s);
-    //     localStorage.setItem('pass_local', password_s);
-    //     localStorage.setItem('pass2_local', password2_s);
-    //     loginChange();
-    //     pageChange();
-    // }
-// }
-
-
-// function check(email){
-//     let filter = arr.filter(function(ele){
-//         return email === ele.Email;
-//     })
-
-//     if(filter.length > 0){
-//         return false;
-//     } else {
-//         return true;
-//     }
-// }
-
-// function check2(pass_1, pass_2){
-
-//     let filter1 = arr.filter(pass_1)
-//     let filter2 = arr.filter(pass_2)
-
-//     if(filter1 !== filter2){
-//         alert('no mathch')
-//     }
+    let obj = {
+        firstName: document.getElementById('firstName').value,
+        lastName: document.getElementById('lastName').value,
+        Email : document.getElementById('email').value,
+        pass1 : document.getElementById('password').value,
+        pass2 : document.getElementById('password2').value
+    }
 
     
-// }
+    if (obj.pass1 !== obj.pass2){
+        alert("❌ pass not match")
+    } else if(check(obj.Email)===true){
+        arr.push(obj);
+        localStorage.setItem("person", JSON.stringify(arr));
+        alert("✔️ account created sucessfully !, redirecting to login");
+        login_togg.style.display = 'flex';
+        singup_togg.style.display = 'none';
 
+    } else {
+        alert("account already exist");
+    }    
+}
+
+
+
+// ========== check for existing account ============ //
+
+
+function check(email){
+    let filter = arr.filter(function(ele){
+        return email === ele.Email;
+    })
+
+
+    if(filter.length > 0){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+// ================================= log in ================================== //
+
+
+
+
+function login(){
+
+    var counter = 0;
+
+    const email_id = document.getElementById('email_L');
+    const pass_id = document.getElementById('password_L');
+
+
+    const Email_input = email_id.value;
+    const Pass_input = pass_id.value;
+
+
+    if(Email_input === ""){
+        email_id.style.borderBottomColor = '1px solid red'
+        alert('⚠️ please fill Email..!!')
+    } 
+    if (Pass_input === "") {
+        email_id.style.borderBottomColor = '1px solid red'
+        alert('⚠️ please fill Password..!!')
+    }
+
+  
+
+    for(let i in arr){
+
+        if(arr[i].Email === Email_input && arr[i].pass1 === Pass_input){ 
+            counter = 1;
+            greetingFn(arr[i].firstName, arr[i].lastName);
+        }
+        else if (arr[i].Email === Email_input && arr[i].pass1 !== Pass_input){      
+            counter = 3;
+        }
+        else if (arr[i].Email !== Email_input && counter !== 1 && counter !== 3 && counter !== 2 ){
+            counter = 2;   
+        } 
+            
+    }
+
+    if(counter === 1){
+        login_togg.style.display = 'none';
+        singup_togg.style.display = 'none';
+        dashboard.style.display = "flex"
+        alert("✔️ Welcome, log in sucessfully !,");    
+    } 
+    else if (counter === 2){
+        alert("❌ Account not found");     
+    }
+    else if(counter === 3){
+        alert("❌ pass incorrect")        
+    }
+}
+
+
+function greetingFn(fName, lName){
+
+    var fullName = fName + " " + lName;
+    welcome_greeting.innerText = fullName;
+    welcome_greeting.style.fontSize = "1.5rem"
+}
